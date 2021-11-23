a9fc382b30e5b3a1ffd8cb2061991ddea091be08
Koji Tsuboi <ktsuboi8@gmail.com>
2021-11-23 21:36:32 +0900

    環境変数設定を追加

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/times', (req, res) => res.send(countHitsuji()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+countHitsuji = () => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      result += '羊が' + i + '匹 '
+    }
+    return result;
+  }
